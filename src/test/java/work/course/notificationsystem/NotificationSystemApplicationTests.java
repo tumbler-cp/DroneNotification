f97f2bbad932ad2bde9bc141f9c85f158ae9d909
arahnik <abdujalol04@mail.ru>
2025-01-10 18:10:31 +0300

    init

---
+++ src/test/java/work/course/notificationsystem/NotificationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package work.course.notificationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationSystemApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
